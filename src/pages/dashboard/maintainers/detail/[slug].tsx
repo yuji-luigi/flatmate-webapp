802dfde7c4802d41214d0e3fbafd8458d587af5d
Yuji Sato <yujisato.usk.jpn@gmail.com>
2023-09-10 14:29:07 +0200

    un used code

--- src/pages/dashboard/maintainers/detail/[slug].tsx
+++ src/pages/dashboard/maintainers/detail/[slug].tsx
@@ -1,56 +1,28 @@
-import {
-  Box,
-  Card,
-  Container,
-  Title,
-  createStyles,
-  em,
-  getBreakpointValue,
-  rem,
-  Text,
-  Group,
-  Avatar,
-  Stack,
-  Divider,
-  Button,
-  SelectItem,
-  Select,
-} from '@mantine/core';
+import { Box } from '@mantine/core';
 import React, { ReactElement, useEffect, useState } from 'react';
 import Layout from '../../../../layouts';
 import ProfileCover, { CoverDataProp } from '../../../../components/profile/ProfileCover';
 import { useCrudSelectors, useCrudSliceStore } from '../../../../redux/features/crud/crudSlice';
 import { useRouter } from 'next/router';
 import { getEntityFromUrl } from '../../../../utils/helpers/helper-functions';
-import { Sections } from '../../../../types/general/data/sections-type';
 import useSWR from 'swr';
 import axiosInstance from '../../../../utils/axios-instance';
 import { PATH_API } from '../../../../path/api-routes';
 import AboutCard from '../../../../components/profile/side/AboutCard';
 import { useMediaQuery } from '@mantine/hooks';
 import ProfileSide from '../../../../components/profile/side/ProfileSide';
-import Image from 'next/image';
-import { PATH_IMAGE, RANDOM_UPLOAD_MODELS } from '../../../../lib/image-paths';
-import AttachmentsRow from '../../../../components/posts/AttachmentsRow';
-import { Icons } from '../../../../data/icons';
+import { RANDOM_UPLOAD_MODELS } from '../../../../lib/image-paths';
 import PostFeedCard from '../../../../components/posts/feed/PostFeedCard';
 import { lorem100 } from '../../../../_mock/strings';
 import { maintainersTableData } from '../../../../../json/dataTable/formfields/maintainersTableData';
 import { use_ModalContext } from '../../../../context/modal-context/_ModalContext';
-import CrudSelect from '../../../../components/input/crud-inputs/CrudSelect';
 import AddMaintainerModal from '../../../../sections/dashboard_pages/maintenance_detail_page/AddMaintainerModal';
-import CardWithTitle from '../../../../components/profile/side/CardWithTitle';
-import TextWithIcon from '../../../../components/text/TextWithIcon';
+
 import { profilePageStyle } from '../../../../styles/global-useStyles';
 import { UserModel } from '../../../../types/models/user-model';
 import { BuildingCard } from '../../../../sections/dashboard_pages/maintainer_detail_page/BuildingCard';
 import { AddRemoveButton } from '../../../../sections/dashboard_pages/maintainer_detail_page/AddRemoveButton';
-const spaceFormField = {
-  id: 'space',
-  label: 'Space',
-  required: true,
-  helperText: 'Select a space',
-};
+
 const useStyles = profilePageStyle;
 
 const getMaintainer = async (slug?: string) => {
@@ -63,10 +35,7 @@ const MaintainerDetailsPage = () => {
   const { cx, classes, theme } = useStyles();
   const router = useRouter();
 
-  const [space, setSpace] = useState<string | null>(null);
-  // const ref = React.useRef<HTMLInputElement>(null);
-
-  const { openModal, openConfirmModal } = use_ModalContext();
+  const { openConfirmModal } = use_ModalContext();
 
   const _entity = getEntityFromUrl();
   const {
@@ -123,9 +92,6 @@ const MaintainerDetailsPage = () => {
       contents={
         <>
           <AddRemoveButton onClick={handleAddMaintainer} />
-          {/* <Button onClick={handleAddMaintainer} variant="outline" color="yellow">
-            Add Maintainer to Building
-          </Button> */}
           <AboutCard aboutData={aboutData} />
           <BuildingCard />
         </>
