5424566089ae7486f6a093f517d3788dd9b4a173
Yuji Sato <yujisato.usk.jpn@gmail.com>
2023-09-18 06:38:23 +0200

    nivo cjs mjs mismatch fix

--- src/sections/dashboard_pages/space_home_section/sections-in-tabs/DashboardSection.tsx
+++ src/sections/dashboard_pages/space_home_section/sections-in-tabs/DashboardSection.tsx
@@ -9,54 +9,14 @@ import { Box } from '@mantine/core';
 //   Tooltip,
 //   ResponsiveContainer,
 // } from 'recharts';
+import dynamic from 'next/dynamic';
 
-import { ResponsiveLine } from '@nivo/line';
-import StackedAreaChart from '../statistics/StacjedAreaChart';
+const StackedAreaChart = dynamic(() => import('../statistics/StacjedAreaChart'), {
+  ssr: false, // This will load the component only on the client side.
+});
+// import { ResponsiveLine } from '@nivo/line';
+// import StackedAreaChart from '../statistics/StacjedAreaChart';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 const DashboardSection = () => {
   // return <Box>DashboardSection</Box>;
   // return <StackedAreaChart />;
