fe191a0f316970d30d1b33b7a14a334f6c39b0ee
Yuji Sato <yujisato.usk.jpn@gmail.com>
2023-10-08 16:15:57 +0200

    typing

--- src/components/posts/feed/FeedAvatar.tsx
+++ src/components/posts/feed/FeedAvatar.tsx
@@ -1,15 +1,14 @@
 import { Avatar } from '@mantine/core';
 import React from 'react';
 import { UserModel } from '../../../types/models/user-model';
-type FeedAvatarProps {
-  createdBy?: UserModel
-}
+
+type FeedAvatarProps = {
+  createdBy?: UserModel;
+};
 export const FeedAvatar = (props: FeedAvatarProps) => {
-  const {createdBy} = props
-  
-  return     <Avatar
-  src={createdBy?.avatar?.url || 'https://picsum.photos/410/300'}
-  radius={90}
-  size={40}
-/>
+  const { createdBy } = props;
+
+  return (
+    <Avatar src={createdBy?.avatar?.url || 'https://picsum.photos/410/300'} radius={90} size={40} />
+  );
 };
