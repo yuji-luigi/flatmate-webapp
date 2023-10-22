376d41073da362fa5b23b9e6506a6d41f35f1a19
Yuji Sato <yujisato.usk.jpn@gmail.com>
2023-10-22 17:53:38 +0200

    posts/postId error logs

--- src/pages/dashboard/posts/[postId].tsx
+++ src/pages/dashboard/posts/[postId].tsx
@@ -45,6 +45,7 @@ const PostIdPage = ({ thread }: { thread: ThreadModel }) => {
 PostIdPage.getLayout = function getLayout(page: ReactElement) {
   return <Layout variant="dashboard">{page}</Layout>;
 };
+
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
   const jwtToken = context.req.cookies.jwt;
   try {
@@ -75,9 +76,22 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
         thread,
       },
     };
-  } catch (error) {
+  } catch (error: any) {
     // log error and send to posts page
     console.error(error);
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error('Response data:', error.response.data);
+      console.error('Response status:', error.response.status);
+      console.error('Response headers:', error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error('Request data:', error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error('Error message:', error.message);
+    }
     return {
       redirect: {
         destination: '/dashboard/posts',
