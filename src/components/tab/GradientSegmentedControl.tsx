6e5bf5616a10fe9c489ea4b74f81b4bcb81f0b8e
Yuji Sato <yujisato.usk.jpn@gmail.com>
2023-12-31 07:26:47 +0100

    Update GradientSegmentedControl component

--- src/components/tab/GradientSegmentedControl.tsx
+++ src/components/tab/GradientSegmentedControl.tsx
@@ -1,34 +1,37 @@
 import { SegmentedControl, SegmentedControlItem } from '@mantine/core';
 import { useEffect, useState } from 'react';
+import { useLocalStorage } from '@mantine/hooks';
 import classes from './GradientSegmentedControl.module.css';
 import { useSegmentedControl } from './useSegmentedControl';
 
 type Props = {
-  data: SegmentedControlItem[];
+  switchDataList: SegmentedControlItem[];
+  localStorageKey?: string;
 };
 export function GradientSegmentedControl(props: Props) {
-  const { data } = props;
-  const { setCurrentValue } = useSegmentedControl();
+  const { switchDataList, localStorageKey } = props;
+  const { setCurrentValue, currentValue } = useSegmentedControl();
+  const [storageValue, setStorageValue] = useLocalStorage({ key: localStorageKey || '' });
   const handleChange = (value: string) => {
     setCurrentValue(value);
+    console.log('value', value);
+    if (localStorageKey) {
+      setStorageValue(value);
+    }
   };
   useEffect(() => {
-    setCurrentValue(data[0].value);
-  }, []);
-  // const [query, setQuery] = useState<string>(data[0].value);
-  // const handleChange = (value: string) => {
-  //   setQuery(value);
-  //   if (value === 'Advanced') {
-  //     console.log('open date picker and set advanced date range');
-  //     // then call api to get data
-  //   }
-  // };
+    if (!storageValue) {
+      // the default value is the first item
+      setCurrentValue(switchDataList[0].value);
+    }
+  }, [storageValue]);
   return (
     <SegmentedControl
       radius="xl"
       size="xs"
       onChange={handleChange}
-      data={data}
+      data={switchDataList}
+      value={currentValue}
       classNames={classes}
     />
   );
