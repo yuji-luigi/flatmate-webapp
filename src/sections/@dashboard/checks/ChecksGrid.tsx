73e3aaedb62047236c995fe2f5b28bcab4e2f2e5
Yuji Sato <yujisato.usk.jpn@gmail.com>
2024-01-06 18:42:34 +0100

    [ADD NEW]Add ChecksGrid component and CSS styles

---
+++ src/sections/@dashboard/checks/ChecksGrid.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { CheckInterface } from '../../../types/models/maintenance-check-type';
+import { PreviewHandler } from '../../../components/files/preview/PreviewHandler';
+import classes from './ChecksGrid.module.css';
+
+export function ChecksGrid({ checks }: { checks: CheckInterface[] }) {
+  return (
+    <div className={classes.grid}>
+      {checks.map((check, i) => (
+        <div className={classes.receipts} key={check._id}>
+          <div className={classes.receiptType}>
+            {check.type}
+            {check.createdAt.toLocaleString()}
+          </div>
+          {check.files.map((file) => (
+            <div key={file._id} className={classes.flex}>
+              <PreviewHandler key={file._id} enableLink file={file} />
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
