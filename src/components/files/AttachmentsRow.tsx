ef691319393e698ee656428340cb737b66a06a87
Yuji Sato <yujisato.usk.jpn@gmail.com>
2024-01-06 18:35:44 +0100

    [UPDATE]AttachmentRow: separate components

--- src/components/files/AttachmentsRow.tsx
+++ src/components/files/AttachmentsRow.tsx
@@ -2,40 +2,10 @@ import { Box, Group, Tooltip } from '@mantine/core';
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { FileImage } from './FileImage';
 import { PATH_IMAGE } from '../../lib/image-paths';
 import { LinkedImage } from '../image/LinkedImage';
 import { UploadModel } from '../../types/models/upload-model';
-
-const ICON_INDEXES = {
-  // Image extensions
-  png: 'image',
-  jpg: 'image',
-  jpeg: 'image',
-  gif: 'image',
-  bmp: 'image',
-  webp: 'image',
-  // Document extensions
-  pdf: 'pdf',
-  doc: 'doc',
-  docx: 'doc',
-  xls: 'xls',
-  xlsx: 'xls',
-  ppt: 'doc',
-  pptx: 'doc',
-  // Video extensions
-  mp4: 'doc',
-  mov: 'doc',
-  avi: 'doc',
-  // Audio extensions
-  mp3: 'doc',
-  wav: 'doc',
-} as const;
-
-function getFileIcon(uploadModel: UploadModel) {
-  const INDEX = ICON_INDEXES[uploadModel.extension];
-
-  return PATH_IMAGE.FILES[INDEX];
-}
 
 const AttachmentsRow = ({
   attachments,
@@ -48,11 +18,7 @@ const AttachmentsRow = ({
     <Group justify="right">
       <Group justify="right">
         {attachments?.map((attachment) => (
-          <Tooltip key={attachment._id} label={attachment.originalFileName}>
-            <Box>
-              <LinkedImage href="#" src={getFileIcon(attachment)} size={30} />
-            </Box>
-          </Tooltip>
+          <FileImage href={attachment.url} key={attachment._id} attachment={attachment} />
         ))}
         {/* <LinkedImage size={30} src={PATH_IMAGE.FILES.doc} href={PATH_IMAGE.FILES.doc} alt="doc" />
       <LinkedImage size={30} src={PATH_IMAGE.FILES.pdf1} href={PATH_IMAGE.FILES.pdf1} alt="pdf1" />
---
+++ src/components/files/FileImage.tsx
@@ -0,0 +1,55 @@
+import { Box, Tooltip } from '@mantine/core';
+import React from 'react';
+
+import { PATH_IMAGE } from '../../lib/image-paths';
+import { UploadModel } from '../../types/models/upload-model';
+import { LinkedImage } from '../image/LinkedImage';
+
+const ICON_INDEXES = {
+  // Image extensions
+  png: 'image',
+  jpg: 'image',
+  jpeg: 'image',
+  gif: 'image',
+  bmp: 'image',
+  webp: 'image',
+  // Document extensions
+  pdf: 'pdf',
+  doc: 'doc',
+  docx: 'doc',
+  xls: 'xls',
+  xlsx: 'xls',
+  ppt: 'doc',
+  pptx: 'doc',
+  // Video extensions
+  mp4: 'doc',
+  mov: 'doc',
+  avi: 'doc',
+  // Audio extensions
+  mp3: 'doc',
+  wav: 'doc',
+} as const;
+
+export function getFileIcon(uploadModel: UploadModel) {
+  const INDEX = ICON_INDEXES[uploadModel.extension];
+
+  return PATH_IMAGE.FILES[INDEX];
+}
+
+export function FileImage({
+  attachment,
+  withTooltip,
+  href,
+}: {
+  href: string;
+  attachment: UploadModel;
+  withTooltip?: boolean;
+}) {
+  return (
+    <Box>
+      <Tooltip hidden={!!withTooltip} key={attachment._id} label={attachment.originalFileName}>
+        <LinkedImage href={href} src={getFileIcon(attachment)} size={30} />
+      </Tooltip>
+    </Box>
+  );
+}
