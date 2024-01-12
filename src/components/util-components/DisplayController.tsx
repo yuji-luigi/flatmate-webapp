f0f4c3049819855838b82957f32600786d6be3eb
Yuji Sato <yujisato.usk.jpn@gmail.com>
2024-01-12 06:38:43 +0100

    Add DisplayController component and its CSS module: hide children when sm

---
+++ src/components/util-components/DisplayController.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './DisplayController.module.css';
+
+const DisplayController = ({ children }) => {
+  return <div className={classes.container}>{children}fsas</div>;
+};
+
+export default DisplayController;
