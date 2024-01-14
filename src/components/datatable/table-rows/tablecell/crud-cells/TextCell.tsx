c1be8794f269b5546b1adb9adf2595df0e0d6454
Yuji Sato <yujisato.usk.jpn@gmail.com>
2024-01-14 22:07:17 +0100

    TextCell > static-select translation logic

--- src/components/datatable/table-rows/tablecell/crud-cells/TextCell.tsx
+++ src/components/datatable/table-rows/tablecell/crud-cells/TextCell.tsx
@@ -1,4 +1,4 @@
-import { Group, Text } from '@mantine/core';
+import { Box, Group, Text } from '@mantine/core';
 import React from 'react';
 import {
   FormFieldTypes,
@@ -7,6 +7,7 @@ import {
   TextFormType,
 } from '../../../../../types/general/data/data-table/formField-types';
 import classes from './Cell.module.css';
+import { useLocale } from '../../../../../../hooks/useLocale';
 
 export const TextCell = ({
   cellValue,
@@ -19,29 +20,28 @@ export const TextCell = ({
 }) => {
   // console.log(cellValue);
   // return cellValue;
+  const { t, locale } = useLocale();
   let displayValue = (
     <Text className={classes.text} size="sm">
       {cellValue}
     </Text>
   );
-
+  // todo: create dedicated component for static select
   if (cellConfig.type === 'static-select') {
     const Icon = cellConfig.options?.find(
       (option: StaticOption) => option.value === cellValue
     )?.icon;
     const dataHidden = !!(cellConfig.type === 'static-select' && Icon);
-
     const text = cellConfig.options?.find(
       (option: StaticOption) => option.value === cellValue
     )?.label;
-
     displayValue = (
-      <Group>
+      <Box className={classes.staticSelectBox}>
         {Icon && <Icon className={classes.icon} />}
         <Text className={classes.text} data-hidden={dataHidden}>
-          {text}
+          {t(text)}
         </Text>
-      </Group>
+      </Box>
     );
   }
 
