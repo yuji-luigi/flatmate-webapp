a24b005fcc1582c294b15fdc29feac0169c73567
Yuji Sato <yujisato.usk.jpn@gmail.com>
2024-01-25 08:10:55 +0100

    TODO: translation in [linkId]/[id].tsx for uploading file

--- src/pages/auth-tokens/maintainer-upload-files/[linkId]/[id].tsx
+++ src/pages/auth-tokens/maintainer-upload-files/[linkId]/[id].tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import { ReactElement, useEffect, useState } from 'react';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { PATH_API, _PATH_API } from '../../../../path/path-api';
 
 import { PinVerifCardMCheck } from '../../../../sections/@nonce-check/m-file-upload/pin-verif/PinVerifCardMCheck';
@@ -40,3 +41,12 @@ MaintainerUploadFileAuthPage.getLayout = function getLayout(page: ReactElement) 
 };
 
 export default MaintainerUploadFileAuthPage;
+
+// export async function getStaticProps({ locale }: { locale: string }) {
+//   return {
+//     props: {
+//       ...(await serverSideTranslations(locale, ['common', 'notification'], null, ['en', 'it'])),
+//       // Will be passed to the page component as props
+//     },
+//   };
+// }
