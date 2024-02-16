8067ba9486ee31d132eb5a090f4f9cc00e6414d3
Yuji Sato <yujisato.usk.jpn@gmail.com>
2024-02-16 20:32:49 +0100

    unused hook useNotifications.tsx

---
+++ hooks/useNotifications.tsx
@@ -0,0 +1,17 @@
+import { NotificationData, NotificationsStore, showNotification } from '@mantine/notifications';
+import { useLocale } from './useLocale';
+
+type useNotificationsProps = { message: string; title: string };
+
+export const useNotifications = () => {
+  const { t } = useLocale();
+  const notification = {
+    show: (data: NotificationData, store: NotificationsStore) => {
+      const message = typeof data.message === 'string' ? t(data.message) : data.message;
+      const title = typeof data.title === 'string' ? t(data.title) : data.title;
+      showNotification({ ...data, message, title }, store);
+    },
+  };
+
+  return notification;
+};
