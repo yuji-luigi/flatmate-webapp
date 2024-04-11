29ea29ec7b27287afae1b408f7438eaef4b12538
Yuji Sato <yujisato.usk.jpn@gmail.com>
2024-04-11 22:51:05 +0200

    fix(vari): vari

--- src/pages/system/dataTable/[entity]/index.tsx
+++ src/pages/system/dataTable/[entity]/index.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import { useRouter } from "next/router";
 import { Box } from "@mantine/core";
 import SystemAdminDashboardLayout from "../../../../layouts/system-admin-dashboard-layout/SystemAdminDashboardLayout";
@@ -7,14 +7,30 @@ import useRouterWithCustomQuery from "../../../../hooks/useRouterWithCustomQuery
 import { CrudDataTable } from "../../../../components/datatable/CrudDataTable";
 import { CrudDrawerDefault } from "../../../../components/drawer/CrudDrawerDefault";
 import { TableSectionHeader } from "../../../../sections/dashboard/datatable_section/TableSectionHeader";
+import { useCurrentEntityContext } from "../../../../context/CurrentEntityContext";
+import { usePaginationContext } from "../../../../context/PaginationContext";
+import { useGetCrudDocuments } from "../../../../hooks/useGetCrudDocuments";
+import { useCrudSliceStore } from "../../../../redux/features/crud/crudSlice";
+import Entity from "../../../dashboard/[entity]";
 
 type SystemDashboardPageProps = {
   getLayout: (page: ReactElement) => ReactElement;
 };
 
 const SystemTablePage = (props: SystemDashboardPageProps) => {
-  const router = useRouterWithCustomQuery();
-  const { entity } = router.query;
+  const { query } = useRouterWithCustomQuery();
+  const { entity } = query;
+  const { paginationQuery } = usePaginationContext();
+  useGetCrudDocuments({ entity, withPagination: true });
+  const { fetchCrudDocumentsWithPagination } = useCrudSliceStore();
+  useEffect(() => {
+    /** type guard */
+    if (!entity) {
+      return;
+    }
+    /** fetch all the entity if not childrenpage */
+    fetchCrudDocumentsWithPagination({ entity, query: paginationQuery });
+  }, [paginationQuery, entity, query.parentId]);
   return (
     <Page title={entity}>
       <Box
