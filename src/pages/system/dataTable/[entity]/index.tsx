75645a6a84b137518940a4fc52823e964051ec61
Yuji Sato <yujisato.usk.jpn@gmail.com>
2024-04-11 08:32:11 +0200

    fix(system admin page): table page adding

--- src/pages/system/dataTable/[entity]/index.tsx
+++ src/pages/system/dataTable/[entity]/index.tsx
@@ -1,14 +1,19 @@
 import { ReactElement } from "react";
+import { useRouter } from "next/router";
 import SystemAdminDashboardLayout from "../../../../layouts/system-admin-dashboard-layout/SystemAdminDashboardLayout";
+import Page from "../../../../components/Page";
+import useRouterWithCustomQuery from "../../../../hooks/useRouterWithCustomQuery";
 
 type SystemDashboardPageProps = {
   getLayout: (page: ReactElement) => ReactElement;
 };
 
-const SystemDashboardPage = (props: SystemDashboardPageProps) => {
-  return <>jey</>;
+const SystemTablePage = (props: SystemDashboardPageProps) => {
+  const router = useRouterWithCustomQuery();
+  const { entity } = router.query;
+  return <Page title={entity}>jey</Page>;
 };
-SystemDashboardPage.getLayout = function getLayout(page: ReactElement) {
+SystemTablePage.getLayout = function getLayout(page: ReactElement) {
   return <SystemAdminDashboardLayout>{page}</SystemAdminDashboardLayout>;
 };
-export default SystemDashboardPage;
+export default SystemTablePage;
