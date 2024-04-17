b58226006c23a2667a86c9af4aa39aed5ec030cf
Yuji Sato <yujisato.usk.jpn@gmail.com>
2024-04-17 07:53:02 +0200

    fix(enhancement): login button when user is system_admin reset the loggedAs to start from regular dashboard

--- src/layouts/homepage/nav/header/LoginButton.tsx
+++ src/layouts/homepage/nav/header/LoginButton.tsx
@@ -6,6 +6,9 @@ import { useRouter } from "next/router";
 import { PATH_CLIENT } from "../../../../path/path-frontend";
 import useAuth from "../../../../../hooks/useAuth";
 import { useCookieContext } from "../../../../context/CookieContext";
+import { _PATH_API } from "../../../../path/path-api";
+import axiosInstance, { AxiosResDataGeneric } from "../../../../utils/axios-instance";
+import { MeUser } from "../../../../types/models/space-model";
 
 const isChoosePage = (path: string) =>
   path === PATH_CLIENT.chooseRootSpace || path === PATH_CLIENT.chooseOrganization;
@@ -15,13 +18,22 @@ const isChoosePage = (path: string) =>
  * @description regular login button. color dark
  */
 export const LoginButton = (props?: ButtonProps) => {
-  const { pathname } = useRouter();
-
+  const { pathname, push } = useRouter();
+  const { user, updateUser } = useAuth();
+  const handleLogin = async () => {
+    if (user?.loggedAs === "system_admin") {
+      const rawRes = await axiosInstance.get<AxiosResDataGeneric<MeUser>>(
+        _PATH_API.auth.systemAdminExit
+      );
+      updateUser(rawRes.data.data);
+    }
+    push(PATH_CLIENT.login);
+  };
   if (pathname === PATH_CLIENT.login) {
     return null;
   }
   return (
-    <Button component={Link} variant={props?.variant || "default"} href={PATH_CLIENT.login}>
+    <Button onClick={handleLogin} variant={props?.variant || "default"}>
       Log in
     </Button>
   );
