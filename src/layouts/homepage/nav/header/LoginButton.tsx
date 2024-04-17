730048ad1fc0e1c0a7efbbf771f058817e78c008
Yuji Sato <yujisato.usk.jpn@gmail.com>
2024-04-17 08:44:35 +0200

    fix: Refactor LoginButton component in homepage/nav/header/LoginButton.tsx

--- src/layouts/homepage/nav/header/LoginButton.tsx
+++ src/layouts/homepage/nav/header/LoginButton.tsx
@@ -1,17 +1,10 @@
-import React from "react";
 import { Button, ButtonProps } from "@mantine/core";
-import Link from "next/link";
-import { NextRequest } from "next/server";
 import { useRouter } from "next/router";
 import { PATH_CLIENT } from "../../../../path/path-frontend";
 import useAuth from "../../../../../hooks/useAuth";
-import { useCookieContext } from "../../../../context/CookieContext";
 import { _PATH_API } from "../../../../path/path-api";
 import axiosInstance, { AxiosResDataGeneric } from "../../../../utils/axios-instance";
 import { MeUser } from "../../../../types/models/space-model";
-
-const isChoosePage = (path: string) =>
-  path === PATH_CLIENT.chooseRootSpace || path === PATH_CLIENT.chooseOrganization;
 
 /**
  *
