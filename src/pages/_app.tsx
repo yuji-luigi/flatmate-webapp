2577df888d8e483bfaf7e0639a29e7ade0ef5924
Yuji Sato <yujisato.usk.jpn@gmail.com>
2024-05-18 10:14:42 +0200

    style: added empty base.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,6 +33,7 @@ import "../styles/test.css";
 import "../styles/nprogress.css";
 import "../styles/components.css";
 import "../styles/layout.css";
+import "../styles/base.css";
 import { useLocale } from "../../hooks/useLocale";
 import { NprogressBar } from "../components/progress-bar/NprogressBar";
 import { CurrentEntityContextProvider } from "../context/CurrentEntityContext";
