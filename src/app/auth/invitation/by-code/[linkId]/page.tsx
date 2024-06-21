098142eaba4181b239cb016eb6e7dedbc3d7aa57
Yuji Sato <yujisato.usk.jpn@gmail.com>
2024-06-21 10:10:25 +0200

    fix: to use redirect in page component needed to remove try catch block

--- src/app/auth/invitation/by-code/[linkId]/page.tsx
+++ src/app/auth/invitation/by-code/[linkId]/page.tsx
@@ -1,28 +1,106 @@
-import axiosInstance from "../../../../../utils/axios-instance";
+import axiosInstance, {
+  AxiosMeResponse,
+  AxiosResDataGeneric,
+} from "../../../../../utils/axios-instance";
 import { apiEndpoint } from "../../../../../path/path-api";
 import { MeUser } from "../../../../../types/models/space-model";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { InvitationByCodePageWrapper } from "./InvitationByCodePageWrapper";
-import { useParams } from "next/navigation";
+import { redirect } from "next/navigation";
+import { _PATH_FRONTEND } from "../../../../../path/path-frontend";
+import LoadingScreen from "../../../../../components/screen/LoadingScreen";
+import { InvitationAuth } from "../../../../../types/models/invitation-model";
+
+export const metadata = {
+  title: "Success! You have been invited to join the team!",
+  description: "Success! You have been invited to join the team!",
+};
+type Props = {
+  params: { linkId: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+// export async function generateMetadata(
+//   { params, searchParams }: Props,
+//   parent: ResolvingMetadata
+// ): Promise<Metadata> {
+//   // read route params
+//   // const id = params.id;
+//   // fetch data
+//   const todo = await fetch(`https://jsonplaceholder.typicode.com/todos/1`).then((res) =>
+//     res.json()
+//   );
+
+//   // optionally access and extend (rather than replace) parent metadata
+//   const previousImages = (await parent).openGraph?.images || [];
+
+//   return {
+//     title: todo.title,
+//     openGraph: {
+//       images: ["/some-specific-page-image.jpg", ...previousImages],
+//     },
+//   };
+// }
 
 // server side check
-const CheckAcceptInvitationPage = async () => {
+export async function CheckAcceptInvitationPage({ params, searchParams }: Props) {
   const cookieStore = cookies();
   const jwtCookie = cookieStore.get("jwt");
-  const token = jwtCookie ? jwtCookie.value : "";
+  const jwt = jwtCookie ? jwtCookie.value : "";
+  const cookie = jwt ? `jwt=${jwt}` : "";
+  const headersInstance = headers();
+  const host = headersInstance.get("host");
+  const protocol = headersInstance.get("x-forwarded-proto") || "http";
 
-  const response = await axiosInstance
-    .get<MeUser>(apiEndpoint.auth.me, {
-      withCredentials: true,
-      headers: {
-        cookie: `jwt=${token}`,
-      },
-    })
-    .catch((error) => {
-      console.error("Error in CheckAcceptInvitationPage", error);
-      return { data: {} };
-    });
-  return <InvitationByCodePageWrapper initialUser={response.data} />;
-};
+  const pathname = headersInstance.get("x-original-uri") || "/";
+  const searchParamsString = new URLSearchParams(searchParams as Record<string, string>).toString();
+
+  const fullUrl = `${protocol}://${host}${pathname}?${searchParamsString}`;
+  const redirectUrl = encodeURIComponent(fullUrl || "no");
+  const { linkId } = params;
+
+  // try {
+  //   redirect("/404");
+  //   return;
+  if (typeof linkId !== "string") {
+    console.error("linkId is not a string");
+    return redirect("/404");
+  }
+
+  if (jwt && typeof linkId === "string") {
+    // call invitations/accept/:linkId with jwt without calling me.
+    try {
+      await axiosInstance.get<AxiosMeResponse>(apiEndpoint.invitations.acceptByLinkId(linkId), {
+        headers: {
+          cookie,
+        },
+      });
+      // return router.push(_PATH_FRONTEND.auth.invitationAcceptSuccess(linkId));
+      return redirect(_PATH_FRONTEND.auth.invitationAcceptSuccess(linkId));
+      // {
+      //   redirect: {
+      //     destination: _PATH_FRONTEND.auth.invitationAcceptSuccess(linkId),
+      //     permanent: false,
+      //   },
+      // };
+    } catch (error) {
+      // can be a different user jwt so redirect to invitation login
+      // console.error(error);
+    }
+  }
+  const rawInvitation = await axiosInstance.get<AxiosResDataGeneric<InvitationAuth>>(
+    apiEndpoint.invitations.byLinkId(linkId)
+  );
+  if (rawInvitation.data.data.status !== "pending") {
+    return redirect(_PATH_FRONTEND.auth.invitationNonValid);
+  }
+  console.log(_PATH_FRONTEND.auth.invitationLogin);
+  // return redirect("/auth/invitation/login");
+  return redirect(`${_PATH_FRONTEND.auth.invitationLogin}?redirect=${redirectUrl}`);
+  // } catch (error) {
+  //   console.error("Error in CheckAcceptInvitationPage", error);
+  //   return null;
+  // }
+}
 
 export default CheckAcceptInvitationPage;
