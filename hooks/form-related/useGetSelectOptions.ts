8318a4f3c126c0238a7c0396865d830daa4b9e43
Yuji <yujisato.usk.jpn@gmail.com>
2024-06-30 07:59:17 +0200

    fix(enhancement): useGetSelectOptions supports only dynamic content
    dynamic data from api without using redux.

--- hooks/form-related/useGetSelectOptions.ts
+++ hooks/form-related/useGetSelectOptions.ts
@@ -6,6 +6,10 @@ import {
   StaticSelectFormFieldType,
 } from "../../src/types/general/data/data-table/form-field-type/formField-types";
 import { MongooseBaseModel } from "../../src/types/models/mongoose-base-model";
+import { apiEndpoint, crudApiEndpoint } from "../../src/path/path-api";
+import useSWR from "swr";
+import { Entity } from "../../src/types/redux/CrudSliceInterfaces";
+import { useFetch } from "../useFetch";
 // import { useCrudSlice } from '../redux-hooks/useCrudSlice';
 
 export const useGetSelectOptions = (
@@ -13,28 +17,35 @@ export const useGetSelectOptions = (
 ): Array<{ value: string; label: string } | string> | [] => {
   // const options: Array<SelectOption> = [];
   // const [idleDocuments, setIdleDocuments] = useState<SelectOption[]>([]);
+
+  const { data, isLoading, error } = useFetch(crudApiEndpoint[formField._entity].root);
+  console.log(data, "data");
+  if (isLoading) return [];
+  if (error) return [];
+  return data.data.map((document: MongooseBaseModel) => {
+    const label = createLabelFromArrayStr(formField.selectValues!, document);
+    return { value: document._id, label };
+  });
   /** fetch with query,  */
-  const { fetchCrudDocuments } = useCrudSliceStore();
+  // const { fetchCrudDocuments } = useCrudSliceStore();
   const { crudDocuments } = useCrudSelectors(formField._entity);
   // const { fetchCrudDocuments, crudDocuments } = useCrudSlice(formField._entity);
+  // TODO: SET THIS LOGIC IN PARENT COMPONENT
+  // if (formField.type === "select" && formField.inputOptions?.useCachedData) {
+  //   return crudDocuments.map((document: MongooseBaseModel) => {
+  //     const label = createLabelFromArrayStr(formField.selectValues!, document);
+  //     return { value: document._id, label };
+  //   });
+  // }
 
-  useEffect(() => {
-    if (formField.type === "select" && !formField.inputOptions?.useCachedData) {
-      fetchCrudDocuments({
-        entity: formField._entity!,
-        queryObject: formField.query,
-      });
-    }
-  }, [formField]);
-
-  if (formField.type === "select") {
-    /** TODO: to include infinite scrolling? */
-    const options = crudDocuments.map((document: MongooseBaseModel) => {
-      const label = createLabelFromArrayStr(formField.selectValues!, document);
-      return { value: document._id, label };
-    });
-    return options;
-  }
+  // if (formField.type === "select") {
+  //   /** TODO: to include infinite scrolling? */
+  //   const options = crudDocuments.map((document: MongooseBaseModel) => {
+  //     const label = createLabelFromArrayStr(formField.selectValues!, document);
+  //     return { value: document._id, label };
+  //   });
+  //   return options;
+  // }
 
   return [];
 };
