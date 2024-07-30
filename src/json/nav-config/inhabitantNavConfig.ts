9eba264c3a603bb714fa6949b2285d298dbf610c
Yuji <yujisato.usk.jpn@gmail.com>
2024-07-30 23:04:47 +0200

    fix: nav configs

--- src/json/nav-config/inhabitantNavConfig.ts
+++ src/json/nav-config/inhabitantNavConfig.ts
@@ -3,28 +3,28 @@ import { NavConfig } from "../../types/data/json/sections-json";
 import { POSTS, TABLE } from "./nav-path";
 
 export const inhabitantNavConfig: NavConfig[] = [
-  {
-    name: "Board",
-    hide: false,
-    contents: [
-      {
-        title: "Settings",
-        link: "/dashboard/settings",
-        icon: "settings",
-        hide: false,
-      },
-      {
-        title: "Threads",
-        link: POSTS("threads"),
-        icon: "posts",
-        hide: false,
-      },
-      {
-        title: "maintenances",
-        link: TABLE("maintenances"),
-        hide: false,
-        icon: "hammer",
-      },
-    ],
-  },
+  // {
+  //   name: "Board",
+  //   hide: false,
+  //   contents: [
+  //     // {
+  //     //   title: "Settings",
+  //     //   link: "/dashboard/settings",
+  //     //   icon: "settings",
+  //     //   hide: false,
+  //     // },
+  //     // {
+  //     //   title: "Threads",
+  //     //   link: POSTS("threads"),
+  //     //   icon: "posts",
+  //     //   hide: false,
+  //     // },
+  //     // {
+  //     //   title: "maintenances",
+  //     //   link: TABLE("maintenances"),
+  //     //   hide: false,
+  //     //   icon: "hammer",
+  //     // },
+  //   ],
+  // },
 ];
